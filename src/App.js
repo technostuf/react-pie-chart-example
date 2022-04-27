056f3d04725d6083e47da17a243b466f544750f3
Amit Patel <89689746+technostuf@users.noreply.github.com>
2022-04-27 12:17:36 +0530

    react pie chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PieChartComponent from './PieChartComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <PieChartComponent />
+    </div>
+  );
+}
+
+export default App;
